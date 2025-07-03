bf80ab21980c56d2bdecd7f72463ba79daee1dad
이주연 <jy122032@gmail.com>
2025-07-03 12:46:22 +0900

    캐릭터 전역변수 생성

--- G4_Minecraft/App.xaml.cs
+++ G4_Minecraft/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using G4_WPF_PROJECT;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,7 @@ namespace G4_Minecraft
     /// </summary>
     public partial class App : Application
     {
+        public Character mainCharacter = new();
     }
 
 }
